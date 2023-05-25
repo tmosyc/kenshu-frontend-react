6dbc34ae777727e00a11acd780f39993506cc112
toshiki.miyagawa <toshiki.miyagawa@M2023-001-prtadminnoMacBook-Pro.local>
2023-05-25 16:48:13 +0900

    完了ボタンの追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,22 @@
 import classNames from "./App.module.css";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
-// usequeryの非同期関数
 const fetchList = () =>
   fetch("http://localhost:8000/api/tasks").then((res) => res.json());
 
 export const App = () => {
   const queryClient = useQueryClient();
+  const [id, setId] = useState({ id: "", changeType: "" });
+  const [text, setText] = useState("");
+  const now = new Date();
 
-  // データ取得
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["tasks"],
     queryFn: fetchList,
   });
 
-  ///　ボタンが押されたらリクエスト送信
   const mutation = useMutation({
     mutationFn: () => {
       return axios.post("http://localhost:8000/api/tasks");
@@ -25,11 +25,48 @@ export const App = () => {
       queryClient.refetchQueries(["tasks"]);
     },
   });
-  //dataが入っているかの判定
+
   if (isLoading || !data) return <p>Loading...</p>;
+
+  const updateTask = (e, taskId) => {
+    e.preventDefault();
+    setId({ id: "", changeType: "update" });
+    axios({
+      method: "patch",
+      url: `http://localhost:8000/api/tasks/${taskId}`,
+      data: { title: text, finishedAt: null },
+    }).then(() => {
+      mutation.mutate();
+    });
+  };
+
+  const updateText = (taskId, taskTitle) => {
+    if (id.id === taskId && id.changeType === "update") {
+      return (
+        <StrictMode>
+          <form onSubmit={(e) => updateTask(e, taskId)}>
+            <input
+              type="text"
+              placeholder={taskTitle}
+              onChange={(event) => setText(event.target.value)}
+            ></input>
+          </form>
+        </StrictMode>
+      );
+    }
+  };
+
+  const finish = async (taskId: string) => {
+    await axios({
+      method: "patch",
+      url: `http://localhost:8000/api/tasks/${taskId}`,
+      data: { finishedAt: now },
+    });
+    refetch();
+  };
+
   return (
     <StrictMode>
-      {/* タスクの追加ボタン */}
       <ul className={classNames.heading}>
         <button
           className={classNames.button}
@@ -39,10 +76,29 @@ export const App = () => {
         >
           タスクの追加
         </button>
-        {/* データの表示 */}
         {data.tasks.map((task) => (
           <li className={classNames.title} key={task.id}>
-            {task.title}
+            <p
+              className={
+                task.finishedAt !== null ? classNames.completeText : ""
+              }
+            >
+              {task.title}
+            </p>
+            <button
+              className={classNames.updateButton}
+              onClick={() => setId({ id: task.id, changeType: "update" })}
+            >
+              {updateText(task.id, task.title)}
+              編集
+            </button>
+
+            <button
+              className={classNames.completeButton}
+              onClick={() => finish(task.id)}
+            >
+              完了
+            </button>
           </li>
         ))}
       </ul>
